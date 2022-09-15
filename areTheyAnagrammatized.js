da67c898bd2c0e2f9b5b7af9aa068e20db6807f8
ArtiJain02 <42202146+ArtiJain02@users.noreply.github.com>
2022-09-15 21:13:08 +0530

    2^5_Code_Submission_1

---
+++ areTheyAnagrammatized.js
@@ -0,0 +1,18 @@
+//cerner_2^5_2022
+
+// Check whether a particular string is an anagram or not.
+
+const isItAnagrammatized = (string1, string2) => {
+  const arrangeAlphabetically = string => string
+    .toLowerCase()
+    .replace(/[^a-z0-9]/gi, '') // remove all the symbols and keep numbers and letters
+    .split('')
+    .sort()
+    .join('');
+  return arrangeAlphabetically(string1) === arrangeAlphabetically(string2);
+};
+
+isItAnagrammatized("vacation time ", " I am not active.") // true
+isItAnagrammatized("a gentleman", "elegant man") // true
+
+// P.S. Anagram is the words that can be rephrased to make another word
