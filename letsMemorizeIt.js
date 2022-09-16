fe2fcbf190ca7d77d7fff7d84744ea6d9342046c
Arti Jain <42202146+ArtiJain02@users.noreply.github.com>
2022-09-16 18:31:08 +0530

    2^5_Code_Submission_2

---
+++ letsMemorizeIt.js
@@ -0,0 +1,32 @@
+//cerner_2^5_2022
+
+// The concept of Memorization
+// Memoization is a programming technique that attempts to increase a function’s performance by caching its previously computed results.
+// JavaScript objects behave like associative arrays, they are ideal candidates to act as caches.
+
+const memoizedMultiplication = () => {
+    let cache = {};
+    return (value) => {
+        if (value in cache) {
+            console.log('Fetching from cache');
+            return cache[value];
+        } else {
+            console.log('Calculating new results');
+            let result = value * 10;
+            cache[value] = result;
+            return result;
+        }
+    }
+}
+// returned function from memoizedMultiplication
+const newMultiplication = memoizedMultiplication();
+
+newMultiplication(9);
+// output: 
+// Calculating new results
+// 90
+
+newMultiplication(9);
+// output:
+// Fetching from cache
+// 90
