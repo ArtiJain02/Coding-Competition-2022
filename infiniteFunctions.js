ff245b7ee9c4fdcdba070f2e061d1dd803cbc436
Arti Jain <42202146+ArtiJain02@users.noreply.github.com>
2022-09-17 21:07:47 +0530

    2^5_Code_Submission_3

---
+++ infiniteFunctions.js
@@ -0,0 +1,11 @@
+//cerner_2^5_2022
+
+// Infinite functions is a concept in js, where we can pass n number of arguments using spread operator
+
+const addition = (...input) => {
+    let sum = 0;
+    for (let i of input) sum += i;
+    return sum;
+}
+addition(1, 2, 3); // Output: 6
+addition(43, 23, 31, 5, 54); // Output: 156
